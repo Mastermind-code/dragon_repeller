dae26057ad45f8cbf1c4bf9cd2e4abef9e544f18
Adebowale Adam <adebowaleadam0@gmail.com>
2023-12-28 10:38:35 +0100

    completed damage per weapon during attack

--- script.js
+++ script.js
@@ -204,6 +204,10 @@ function attack() {
   } else if (monsterHealth <= 0) {
     fighting === 2 ? winGame() : defeatMonster();
   }
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
+    text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
+  }
 }
 
 function getMonsterAttackValue(level) {
